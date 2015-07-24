d3000113a42921fc5dc278f57b4dcfbb0f802849
BradBolander <bradbolander@BradBolanders-MacBook-Pro.local>
2015-07-24 15:14:42 -0500

    Update

--- app.rb
+++ app.rb
@@ -26,17 +26,17 @@ movie2 = Movie.new('interstellar', '2014', 'SPACE')
 movie3 = Movie.new('kung-fu-hustle', '2004', 'Who doesnt love kung fu?')
 
 get '/' do
-  {:message => 'Try /300, /interstellar, and /kung-fu-hustle !' }.to_json
+  {:message => 'Try /api/300, /api/interstellar, and /api/kung-fu-hustle !' }.to_json
 end
 
-get '/300' do
+get '/api/300' do
   {:name => movie1.get_name, :release_year => movie1.get_release_year, :description => movie1.get_description}.to_json
 end
 
-get '/interstellar' do
+get '/api/interstellar' do
   {:name => movie2.get_name, :release_year => movie2.get_release_year, :description => movie2.get_description}.to_json
 end
 
-get '/kung-fu-hustle' do
+get '/api/kung-fu-hustle' do
   {:name => movie3.get_name, :release_year => movie3.get_release_year, :description => movie3.get_description}.to_json
 end
